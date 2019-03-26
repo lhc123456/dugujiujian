a537b3b9f2a27261a860b84a020e4c6a27f0ea7c
lhc123456 <lhcjy000@163.com>
2019-03-26 19:26:15 +0800

    lhc_commit_newJianfa2

--- src/main/java/NewJianfa.java
+++ src/main/java/NewJianfa.java
@@ -1,5 +1,9 @@
 public class NewJianfa {
     public static void main(String[] args) {
         System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
     }
 }
